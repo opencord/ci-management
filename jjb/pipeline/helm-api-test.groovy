4db8431fce7a1fecc6aa675df48acb645ed576aa
Zack Williams <zdw@opennetworking.org>
2018-05-14 13:00:57 -0700

    [CORD-2966]v2 - fix issues with the first patch of helm-api-test
    Change-Id: I4cb8d4fb0e4eeb33b2b455731370e8333c9a4fad

--- jjb/pipeline/helm-api-test.groovy
+++ jjb/pipeline/helm-api-test.groovy
@@ -2,26 +2,20 @@
 
 pipeline {
 
-  parameters {
-    string(name:'executorNode', defaultValue:'invalid', description:'Name of the Jenkins node to run the job on')
-    string(name:'manifestUrl', defaultValue:'invalid', description:'URL to the repo manifest')
-    string(name:'manifestBranch', defaultValue:'master', description:'Name of the repo branch to use')
-  }
-
   /* no label, executor is determined by JJB */
   agent {
-    label '${params.executorNode}'
+    label "${params.executorNode}"
   }
 
   stages {
 
-    stage('checkout') {
+    stage('repo') {
       steps {
         checkout(changelog: false, \
           poll: false,
           scm: [$class: 'RepoScm', \
-            manifestRepositoryUrl: '${params.manifestUrl}', \
-            manifestBranch: '${params.manifestBranch}', \
+            manifestRepositoryUrl: "${params.manifestUrl}", \
+            manifestBranch: "${params.manifestBranch}", \
             currentBranch: true, \
             destinationDir: 'cord', \
             forceSync: true,
@@ -38,7 +32,7 @@ pipeline {
 
         stage('images') {
           steps {
-            sh 'cd cord/build; ./scripts/imagebuilder.py -f helm-charts/examples/test-images.yaml'
+            sh 'cd cord/build; ./scripts/imagebuilder.py -f ../helm-charts/examples/test-images.yaml'
 
           }
         }
@@ -47,17 +41,14 @@ pipeline {
           steps {
             /* see https://github.com/kubernetes/minikube/#linux-continuous-integration-without-vm-support */
             sh '''
-               export MINIKUBE_WANTUPDATENOTIFICATION=false;
-               export MINIKUBE_WANTREPORTERRORPROMPT=false;
-               export CHANGE_MINIKUBE_NONE_USER=true;
-               export MINIKUBE_HOME=$HOME;
-               mkdir -p $HOME/.kube || true;
-               touch $HOME/.kube/config;
-               export KUBECONFIG=$HOME/.kube/config;
-               minikube start --vm-driver=none;
-
-               chown -R $USER $HOME/.minikube;
-               chgrp -R $(id -g) $HOME/.minikube;
+               export MINIKUBE_WANTUPDATENOTIFICATION=false
+               export MINIKUBE_WANTREPORTERRORPROMPT=false
+               export CHANGE_MINIKUBE_NONE_USER=true
+               export MINIKUBE_HOME=$HOME
+               mkdir -p $HOME/.kube || true
+               touch $HOME/.kube/config
+               export KUBECONFIG=$HOME/.kube/config
+               sudo -E /usr/bin/minikube start --vm-driver=none
 
                for i in {1..150}; do # timeout for 5 minutes
                    ./kubectl get po &> /dev/null
@@ -81,9 +72,9 @@ pipeline {
     stage('xos') {
       steps {
         sh '''
-           cd cord/build/helm-charts;
-           helm dep up xos-core;
-           helm install -f examples/test-values.yaml -f examples/candidate-tag-values.yaml xos-core -n xos-core;
+           cd cord/helm-charts
+           helm dep up xos-core
+           helm install -f examples/test-values.yaml -f examples/candidate-tag-values.yaml xos-core -n xos-core
            sleep 60
            '''
       }
@@ -91,22 +82,32 @@ pipeline {
 
     stage('test'){
       steps {
-        sh 'helm test xos-core'
-        sh 'kubectl logs xos-core-api-test'
-      }
-      post {
-        always {
-          archive '/tmp/helm_test_xos_core_logs_*/**'
+        sh '''
+           helm test xos-core
+           kubectl logs xos-core-api-test
+           mkdir -p ./RobotLogs;
+           cp /tmp/helm_test_xos_core_logs_*/* ./RobotLogs
+           '''
 
-        }
+        step([$class: 'RobotPublisher',
+             disableArchiveOutput: false,
+             logFileName: 'RobotLogs/log*.html',
+             otherFiles: '',
+             outputFileName: 'RobotLogs/output*.xml',
+             outputPath: '.',
+             passThreshold: 100,
+             reportFileName: 'RobotLogs/report*.html',
+             unstableThreshold: 0]);
       }
     }
   }
 
   post {
     always {
-      sh 'kubectl delete pod xos-core-api-test'
-      sh 'helm delete --purge xos-core'
+      sh '''
+         kubectl delete pod xos-core-api-test
+         helm delete --purge xos-core
+         '''
       deleteDir()
     }
   }
