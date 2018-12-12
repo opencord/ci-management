362bfe5a6cede082dcba0299807cabd2e808addc
Andy Bavier <andy@onlab.us>
2018-12-12 15:36:30 -0700

    VOLTHA image publisher job
    Change-Id: I175c058300d6ea03c56d86cd5d325214904bd936

---
+++ jjb/pipeline/voltha-publish.groovy
@@ -0,0 +1,64 @@
+/* voltha-publish pipeline */
+pipeline {
+
+  /* no label, executor is determined by JJB */
+  agent {
+    label "${params.executorNode}"
+  }
+  stages {
+
+    stage('repo') {
+      steps {
+        checkout(changelog: false, \
+          poll: false,
+          scm: [$class: 'RepoScm', \
+            manifestRepositoryUrl: "${params.manifestUrl}", \
+            manifestBranch: "${params.manifestBranch}", \
+            currentBranch: true, \
+            destinationDir: 'cord', \
+            forceSync: true,
+            resetFirst: true, \
+            quiet: true, \
+            jobs: 4, \
+            showAllChanges: true] \
+          )
+      }
+    }
+
+    stage('build'){
+      steps {
+        sh """
+           #!/usr/bin/env bash
+
+           pushd cord/incubator/voltha
+           VOLTHA_BUILD=docker DOCKER_CACHE_ARG=--no-cache TAG=${params.manifestBranch} make build
+
+           popd
+           """
+      }
+    }
+
+    stage('push'){
+      steps {
+        sh """
+           #!/usr/bin/env bash
+
+           pushd cord/incubator/voltha
+           VOLTHA_BUILD=docker TAG=${params.manifestBranch} TARGET_REPOSITORY=voltha/ TARGET_TAG=${params.manifestBranch} make push
+
+           popd
+           """
+      }
+    }
+  }
+
+  post {
+    failure {
+      emailext (
+        subject: "$PROJECT_NAME - Build # $BUILD_NUMBER - $BUILD_STATUS",
+        body: "Check console output at $BUILD_URL to view the results.",
+        to: "${params.failureEmail}"
+      )
+    }
+  }
+}
