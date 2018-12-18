4303a0dbe34af53a5ec221cc7468b0dd89949caf
Kailash <kailash@onlab.us>
2018-12-18 07:49:14 -0800

    building siab with voltha-1.6
    Change-Id: I1ebcf6c5e3a0f890b5366153ee02341f7c40f4cf

--- jjb/pipeline/siab.groovy
+++ jjb/pipeline/siab.groovy
@@ -39,7 +39,7 @@ pipeline {
       steps {
         sh """
             pushd $WORKSPACE/automation-tools/seba-in-a-box
-            make /tmp/milestones/voltha-running && make -j2
+	    make SEBAVALUES=configs/seba-ponsim-voltha-1.6.yaml
             popd
             """
             }
