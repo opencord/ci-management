c5a5242d96a73de8afe2159def617f2bd5c3982d
Kailash <kailash@onlab.us>
2019-03-19 09:21:22 -0700

    fixing syntax bug in bbsimpipeline
    Change-Id: I4b9a7ff12aa78deb0371b2c6b65624a8857e6948

--- jjb/pipeline/bbsim-validation.groovy
+++ jjb/pipeline/bbsim-validation.groovy
@@ -150,12 +150,12 @@ pipeline {
 
     stage('Test BBSIM') {
       steps {
-        sh '''
+        sh """
            #!/usr/bin/env bash
            set -eu -o pipefail
            pushd cord/test/cord-tester/src/test/cord-api/Tests/BBSim/
            robot -e notready -v number_of_onus:${params.OnuCount} BBSIMScale.robot || true
-           '''
+           """
       }
     }
   }
