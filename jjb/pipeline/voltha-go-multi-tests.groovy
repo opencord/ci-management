08eb5901fcb08973f2226c0b3de6fe779ead5b53
Andy Bavier <andy@opennetworking.org>
2020-01-08 13:44:03 -0700

    VOL-2433 Turn off auto SADIS config, pipeline cleanup
    Change-Id: I644e9796cefc8452f60e5121001dce02a466ea56

--- jjb/pipeline/voltha-go-multi-tests.groovy
+++ jjb/pipeline/voltha-go-multi-tests.groovy
@@ -38,7 +38,7 @@ pipeline {
     VOLTHA_LOG_LEVEL="DEBUG"
     CONFIG_SADIS="n"
     EXTRA_HELM_FLAGS="${params.extraHelmFlags}"
-    ROBOT_MISC_ARGS="${params.extraRobotArgs} -d $WORKSPACE/RobotLogs"
+    ROBOT_MISC_ARGS="${params.extraRobotArgs} -d $WORKSPACE/RobotLogs -v teardown_device:False"
   }
   stages {
 
@@ -81,9 +81,51 @@ pipeline {
     stage('Run E2E Tests') {
       steps {
         sh '''
+
+           clear_etcd() {
+             kubectl -n voltha exec $(kubectl -n voltha get pods -lapp=etcd -o=name) -- sh -c "ETCDCTL_API=3 etcdctl del --prefix $1"
+           }
+
+           wait_for_state_change() {
+             timeout 60 bash -c "until voltctl device list -f Type=$2,AdminState=$3,OperStatus=$4,ConnectStatus=$5 -q | wc -l | grep -q 1; do echo Waiting for $1 to change states; voltctl device list; echo; sleep 5; done"
+           }
+
            mkdir -p $WORKSPACE/RobotLogs
            git clone https://gerrit.opencord.org/voltha-system-tests
-           make -C $WORKSPACE/voltha-system-tests ${makeTarget}
+           cd kind-voltha
+           for i in \$(seq 1 ${testRuns})
+           do
+             make -C $WORKSPACE/voltha-system-tests ${makeTarget}
+             echo "Completed run: \$i"
+             echo ""
+             if [[ \$i -lt ${testRuns} ]]
+             then
+               # For testing multiple back-to-back runs
+               # Doing some manual cleanup to work around known issues in BBSim and ONOS apps
+
+               OLT=\$( voltctl device list -f Type=openolt -q )
+               ONU=\$( voltctl device list -f Type=brcm_openomci_onu -q )
+
+               voltctl device disable \$OLT
+               wait_for_state_change OLT openolt DISABLED UNKNOWN REACHABLE
+               wait_for_state_change ONU brcm_openomci_onu ENABLED DISCOVERED UNREACHABLE
+
+               voltctl device disable \$ONU
+               wait_for_state_change ONU brcm_openomci_onu DISABLED UNKNOWN UNREACHABLE
+
+               voltctl device delete \$OLT
+
+               timeout 60 bash -c 'until voltctl device list -q | wc -l | grep -q 0; do echo "Waiting for all devices to be deleted"; voltctl device list; echo ""; sleep 5; done'
+
+               helm delete --purge bbsim # VOL-2342
+               helm delete --purge onos # VOL-2343, VOL-2363
+               clear_etcd service/voltha/resource_manager
+               clear_etcd service/voltha/openolt
+               clear_etcd service/voltha/devices
+               sleep 30
+               DEPLOY_K8S=no ./voltha up  # Will just re-deploy BBSim and ONOS
+             fi
+           done
            '''
       }
     }
