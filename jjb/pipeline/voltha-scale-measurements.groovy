51b7630435ddf71755f9e21184ab1bda45c98965
Matteo Scandolo <matteo.scandolo@gmail.com>
2020-02-05 12:07:23 -0800

    Create skeleton for VOLTHA scale job
    Change-Id: Ifa71282c4f90ef3e8dff2b48434d13033af55d95

---
+++ jjb/pipeline/voltha-scale-measurements.groovy
@@ -0,0 +1,46 @@
+/* voltha-scale-measurements pipeline */
+pipeline {
+
+  /* no label, executor is determined by JJB */
+  agent {
+    label "${params.buildNode}"
+  }
+  stages {
+    stage('checkout') {
+      steps {
+        checkout([
+          $class: 'GitSCM',
+          userRemoteConfigs: [[ url: "https://github.com/ciena/kind-voltha.git", ]],
+          branches: [[ name: "master", ]],
+          extensions: [
+            [$class: 'WipeWorkspace'],
+            [$class: 'RelativeTargetDirectory', relativeTargetDir: "${params.projectName}"],
+            [$class: 'CloneOption', depth: 0, noTags: false, reference: '', shallow: false],
+          ],
+        ])
+        script {
+          git_tags = sh(script:"cd $projectName; git tag -l --points-at HEAD", returnStdout: true).trim()
+        }
+      }
+    }
+    stage('start') {
+      steps {
+        sh """
+          #!/usr/bin/env bash
+          set -euo pipefail
+
+          echo "DO SOMETHING"
+          """
+      }
+    }
+  }
+  post {
+    cleanup {
+      sh '''
+        WAIT_ON_DOWN=y ./voltha down
+        cd $WORKSPACE/
+        rm -rf kind-voltha/ || true
+      '''
+    }
+  }
+}
