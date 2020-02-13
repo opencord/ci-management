8d8c9a3b3444edf43231c6b59c47044907271f14
Andy Bavier <andy@opennetworking.org>
2020-02-12 19:41:08 -0700

    Need kind-voltha in workspace to run scripts
    Change-Id: Ia7c12650d7e4560e7e63a4cf46c853e2e9da9625

--- jjb/pipeline/voltha-physical-functional-tests.groovy
+++ jjb/pipeline/voltha-physical-functional-tests.groovy
@@ -59,6 +59,7 @@ pipeline {
         git clone -b ${branch} ${cordRepoUrl}/cord-tester
         mkdir -p $WORKSPACE/bin
         bash <( curl -sfL https://raw.githubusercontent.com/boz/kail/master/godownloader.sh) -b "$WORKSPACE/bin"
+        git clone https://github.com/ciena/kind-voltha.git
         """
       }
     }
