7b3fde9959545687dfd6252e584d1530f4f2cf3d
Matteo Scandolo <matteo.scandolo@gmail.com>
2020-02-26 16:17:16 -0800

    Extending voltctl timeout to 5 minutes in scale measurement job
    Change-Id: Ia2988dd49a7d28453916d5fa68cc1e0474707620

--- jjb/pipeline/voltha-scale-measurements.groovy
+++ jjb/pipeline/voltha-scale-measurements.groovy
@@ -25,7 +25,7 @@ pipeline {
     stage('set-description') {
       steps {
         script {
-          currentBuild.description = "${onuPerPon} ONU x ${ponPorts} PON"
+          currentBuild.description = "${onuPerPon} ONU x ${ponPorts} PON - BBSIM Delay ${BBSIMdelay}"
         }
       }
     }
@@ -106,6 +106,8 @@ pipeline {
           sshpass -e ssh -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -p 8101 karaf@localhost cfg set org.onosproject.provider.of.flow.impl.OpenFlowRuleProvider flowPollFrequency ${flowStatInterval}
           #Setting the ports stats collection interval
           sshpass -e ssh -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -p 8101 karaf@localhost cfg set org.onosproject.provider.of.device.impl.OpenFlowDeviceProvider portStatsPollFrequency ${portsStatInterval}
+          # extending voltctl timeout
+          sed -i 's/timeout: 10s/timeout: 5m/g' /home/cord/.volt/config-minimal
         '''
       }
     }
