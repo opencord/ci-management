e3af892ad3137274206363b806da8dab8bd8f496
Shrey Baid <shrey1.baid@gmail.com>
2020-03-24 23:02:34 +0530

    [WIP] Using kafka to check for ONU Activation
    Change-Id: I228b5a51b1c210d239c258b956670b9f66597b75

--- jjb/pipeline/voltha-scale-measurements.groovy
+++ jjb/pipeline/voltha-scale-measurements.groovy
@@ -153,18 +153,12 @@ pipeline {
                 echo -e "You need to set the target ONU number\n"
                 exit 1
               fi
-
+              dt=$(date -u +"%s")
               voltctl device create -t openolt -H bbsim:50060
               voltctl device enable $(voltctl device list --filter Type~openolt -q)
-              # check ONUs reached Active State in VOLTHA
-              i=$(voltctl device list | grep -v OLT | grep ACTIVE | wc -l)
-              until [ $i -eq ${expectedOnus} ]
-              do
-                echo "$i ONUs ACTIVE of ${expectedOnus} expected (time: $SECONDS)"
-                sleep ${pollInterval}
-                i=$(voltctl device list | grep -v OLT | grep ACTIVE | wc -l)
-              done
-              echo "${expectedOnus} ONUs Activated in $SECONDS seconds (time: $SECONDS)"
+              export expectedOnus
+              kafkacat=$(kubectl get pods --all-namespaces | grep "kafkacat" | awk '{print $2}')
+              kubectl exec -it $kafkacat -- kafkacat -b cord-kafka -C -t BBSim-OLT-0-Events -o s@$dt | python /home/cord/ONU-detection.py
               echo $SECONDS > voltha-devices-time-num.txt
             '''
           }
