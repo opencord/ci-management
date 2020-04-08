682702adab10b06aec23b314ec1fd18854041b9a
Jeremy Ronquillo <jeremyr@opennetworking.org>
2020-04-08 16:27:55 -0700

    Add quiet period to downstream omec-deploy-staging jobs
    Change-Id: I08673d3e90126b628981f0dfdabe60a902a1eab1

--- jjb/pipeline/omec-postmerge.groovy
+++ jjb/pipeline/omec-postmerge.groovy
@@ -20,6 +20,7 @@ def hss_tag = ""
 def mme_tag = ""
 def spgwc_tag = ""
 def spgwu_tag = ""
+def quietPeriodTime = 0
 
 pipeline {
 
@@ -61,6 +62,11 @@ pipeline {
             mme_tag = "${branchName}-${commitHash}"
             break
           }
+          def now = Math.floor((new Date()).getTime() / 1000.0) // Get current time in seconds
+          def PDTOffset = 25200                                 // PDT Offset from UTC in seconds
+          def oneDay = 86400                                    // 24 hours in seconds
+          quietPeriodTime = oneDay - (now - PDTOffset) % oneDay // number of seconds until next midnight
+          println "Quiet Period (seconds until next midnight): " + quietPeriodTime
         }
         build job: "omec-deploy-staging", parameters: [
               string(name: 'hssdb_tag', value: "${hssdb_tag}"),
@@ -68,7 +74,7 @@ pipeline {
               string(name: 'hss_tag', value: "${mme_tag}"),
               string(name: 'spgwc_tag', value: "${spgwc_tag}"),
               string(name: 'spgwu_tag', value: "${spgwu_tag}"),
-            ]
+            ], quietPeriod: quietPeriodTime
       }
     }
   }
