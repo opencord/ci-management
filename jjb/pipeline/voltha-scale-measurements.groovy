3e0e0405f0a2204e76802fc0faca390395504025
Matteo Scandolo <matteo.scandolo@gmail.com>
2020-04-27 16:07:05 -0700

    Moving the scale job for a single ONU to perform the full scenario
    Change-Id: Ib28c0ba73d59fcfaf470c0bac995039acc2989fd

--- jjb/pipeline/voltha-scale-measurements.groovy
+++ jjb/pipeline/voltha-scale-measurements.groovy
@@ -136,6 +136,7 @@ pipeline {
             sshpass -e ssh -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -p 30115 karaf@127.0.0.1 app deactivate org.opencord.aaa
             sshpass -e ssh -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -p 30115 karaf@127.0.0.1 app deactivate org.opencord.dhcpl2relay
           else
+            # config SADIS
             curl --fail -sSL --user karaf:karaf -X POST -H Content-Type:application/json "http://127.0.0.1:30120/onos/v1/network/configuration/apps/org.opencord.sadis" --data '{
               "sadis": {
                   "integration": {
@@ -156,6 +157,23 @@ pipeline {
                           "ttl": "PT1m"
                       }
                   }
+              }
+            }'
+
+            # config AAA
+            curl --fail -sSL --user karaf:karaf -X POST -H Content-Type:application/json "http://127.0.0.1:30120/onos/v1/network/configuration/apps/org.opencord.aaa" --data '{
+              "AAA": {
+                "radiusConnectionType" : "socket",
+                "radiusHost": "radius.default.svc.cluster.local",
+                "radiusServerPort": "1812",
+                "radiusSecret": "SECRET"
+              }
+            }'
+
+            # config DHCP
+            curl --fail -sSL --user karaf:karaf -X POST -H Content-Type:application/json "http://127.0.0.1:30120/onos/v1/network/configuration/apps/org.opencord.dhcpl2relay" --data '{
+              "dhcpl2relay" : {
+                "useOltUplinkForServerPktInOut": true
               }
             }'
           fi
@@ -331,7 +349,7 @@ pipeline {
               for s in "${subs[@]}"
               do
                 IFS=/ read -r device port <<< $s
-                echo -e "\n Authenticating subscriber on device $device and port $port "
+                echo "Adding subscriber on device $device and port $port"
                 sshpass -e ssh -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -p 30115 karaf@127.0.0.1 volt-add-subscriber-access $device $port
               done
             fi
