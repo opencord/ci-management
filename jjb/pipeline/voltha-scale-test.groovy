ced53da051234ba3768c0da2ef19699d45ce4f77
Matteo Scandolo <matteo.scandolo@gmail.com>
2020-07-15 17:59:40 -0700

    Collecting logs from the new karaf version in the ONOS container
    Change-Id: Ia3b58c8c8caf39dbe188742b4b8172023ad51382

--- jjb/pipeline/voltha-scale-test.groovy
+++ jjb/pipeline/voltha-scale-test.groovy
@@ -434,7 +434,7 @@ EOF
         done
 
         # copy the ONOS logs directly from the container to avoid the color codes
-        printf '%s\n' $(kubectl get pods -l app=onos-onos-classic -o=jsonpath="{.items[*]['metadata.name']}") | xargs -I@ bash -c "kubectl cp @:apache-karaf-4.2.8/data/log/karaf.log $LOG_FOLDER/@.log"
+        printf '%s\n' $(kubectl get pods -l app=onos-onos-classic -o=jsonpath="{.items[*]['metadata.name']}") | xargs -I@ bash -c "kubectl cp @:apache-karaf-4.2.9/data/log/karaf.log $LOG_FOLDER/@.log"
       '''
       // dump all the BBSim(s) ONU informations
       sh '''
