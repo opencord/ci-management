fe24c629e1f2b6f3768db56c8cbfd6c6f765ba53
TorstenThieme <torsten.thieme@adtran.com>
2021-01-26 07:41:47 +0000

    increase timeout due long duration reconcile tests
    Change-Id: I12d74b666da8956c4d0b871755e1202f97c37a4f

--- jjb/pipeline/voltha-openonu-go-test-bbsim.groovy
+++ jjb/pipeline/voltha-openonu-go-test-bbsim.groovy
@@ -23,7 +23,7 @@ pipeline {
     label "${params.buildNode}"
   }
   options {
-    timeout(time: 90, unit: 'MINUTES')
+    timeout(time: 115, unit: 'MINUTES')
   }
   environment {
     KUBECONFIG="$HOME/.kube/kind-config-voltha-minimal"
