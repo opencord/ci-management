98f1d823d5192c21168e53c2be831dc1ff4cc574
Hardik Windlass <hardik@opennetworking.org>
2021-02-11 07:09:56 +0000

    Inc timeout to support bbsim multi-olt jobs
    Change-Id: I6b5ceb1082f6114aee7d2bee91206d05d407aad0

--- jjb/pipeline/voltha-nightly-tests-bbsim.groovy
+++ jjb/pipeline/voltha-nightly-tests-bbsim.groovy
@@ -23,7 +23,7 @@ pipeline {
     label "${params.buildNode}"
   }
   options {
-      timeout(time: 190, unit: 'MINUTES')
+      timeout(time: 240, unit: 'MINUTES')
   }
   environment {
     KUBECONFIG="$HOME/.kube/kind-config-voltha-minimal"
