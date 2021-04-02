660e4af8935f46321397a95d7797955c9fa65f34
Matteo Scandolo <matteo.scandolo@gmail.com>
2021-04-02 11:16:09 -0700

    Supporting multiple ONOSes in volthaInfraDeploy keyword
    Change-Id: Ia67c52b29500f6e15c9f9c7d938b7a30ae42a527

---
+++ vars/getVolthaImageFlags.groovy
@@ -0,0 +1,37 @@
+// returns the helm flags required to override a specific image
+def call(String project = "unknown", String tag = "citest", String pullPolicy = "Never") {
+  def chart = "unknown"
+  def image = "unknown"
+  switch(project) {
+    case "ofagent-go":
+      chart = "voltha"
+      image = "ofagent"
+    break
+    case "voltha-go":
+      chart = "voltha"
+      image = "rw_core"
+    break
+    case "voltha-openonu-adapter-go":
+      chart = "voltha-adapter-openonu"
+      image = "adapter_open_onu_go"
+    break
+    // TODO remove after 2.7
+    case "voltha-openonu-adapter":
+      chart = "voltha-adapter-openonu"
+      image = "adapter_open_onu"
+    break
+    // TODO end
+    case "voltha-openolt-adapter":
+      chart = "voltha-adapter-openolt"
+      image = "adapter_open_olt"
+    break
+    case "bbsim":
+      // BBSIM has a different format that voltha, return directly
+      return "--set images.bbsim.tag=${tag},images.bbsim.pullPolicy=${pullPolicy}"
+    break
+    default:
+    break
+  }
+
+  return "--set ${chart}.images.${image}.tag=${tag},${chart}.images.${image}.pullPolicy=${pullPolicy} "
+}
--- vars/volthaInfraDeploy.groovy
+++ vars/volthaInfraDeploy.groovy
@@ -49,6 +49,7 @@ def call(Map config) {
     // TODO support multiple replicas
     sh """
     helm upgrade --install --create-namespace -n ${cfg.infraNamespace} voltha-infra ${volthaInfraChart} ${cfg.extraHelmFlags} \
+          --set onos-classic.replicas=${cfg.onosReplica},onos-classic.atomix.replicas=${cfg.atomixReplica} \
           -f $WORKSPACE/voltha-helm-charts/examples/${cfg.workflow}-values.yaml
     """
 }
