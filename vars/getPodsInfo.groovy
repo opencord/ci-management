8b62af3ea15c857d8d73c7ba7e305474973b7048
Matteo Scandolo <matteo.scandolo@gmail.com>
2021-05-25 08:41:12 -0700

    Capturing service when getting pods info
    Change-Id: I6231ff54fb4c2e7b6956cda807cf94875d74917b

--- vars/getPodsInfo.groovy
+++ vars/getPodsInfo.groovy
@@ -3,11 +3,16 @@
 def call(String dest) {
   sh """
   mkdir -p ${dest}
+  # only tee the main infos
   kubectl get pods --all-namespaces -o wide | tee ${dest}/pods.txt || true
-  kubectl get pods --all-namespaces -o jsonpath="{range .items[*].status.containerStatuses[*]}{.image}{'\\n'}" | sort | uniq | tee ${dest}/pod-images.txt || true
-  kubectl get pods --all-namespaces -o jsonpath="{range .items[*].status.containerStatuses[*]}{.imageID}{'\\n'}" | sort | uniq | tee ${dest}/pod-imagesId.txt || true
+  helm ls --all-namespaces | tee ${dest}/helm-charts.txt
+
+  # everything else should not be dumped on the console
+  kubectl get svc --all-namespaces -o wide > ${dest}/svc.txt || true
+  kubectl get pvc --all-namespaces -o wide > ${dest}/pvcs.txt || true
+  kubectl get pods --all-namespaces -o jsonpath="{range .items[*].status.containerStatuses[*]}{.image}{'\\n'}" | sort | uniq > ${dest}/pod-images.txt || true
+  kubectl get pods --all-namespaces -o jsonpath="{range .items[*].status.containerStatuses[*]}{.imageID}{'\\n'}" | sort | uniq > ${dest}/pod-imagesId.txt || true
   kubectl describe pods --all-namespaces -l app.kubernetes.io/part-of=voltha > ${dest}/voltha-pods-describe.txt
   kubectl describe pods --all-namespaces -l app=onos-classic > ${dest}/onos-pods-describe.txt
-  helm ls --all-namespaces | tee ${dest}/helm-charts.txt
   """
 }
