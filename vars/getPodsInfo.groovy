b3bac795ac63a7d084abe40c6cd9ebb340a7fc3c
Matteo Scandolo <matteo.scandolo@gmail.com>
2021-05-25 08:41:12 -0700

    Capturing service when getting pods info
    Change-Id: I6231ff54fb4c2e7b6956cda807cf94875d74917b

--- vars/getPodsInfo.groovy
+++ vars/getPodsInfo.groovy
@@ -4,6 +4,7 @@ def call(String dest) {
   sh """
   mkdir -p ${dest}
   kubectl get pods --all-namespaces -o wide | tee ${dest}/pods.txt || true
+  kubectl get svc --all-namespaces -o wide | tee ${dest}/svc.txt || true
   kubectl get pods --all-namespaces -o jsonpath="{range .items[*].status.containerStatuses[*]}{.image}{'\\n'}" | sort | uniq | tee ${dest}/pod-images.txt || true
   kubectl get pods --all-namespaces -o jsonpath="{range .items[*].status.containerStatuses[*]}{.imageID}{'\\n'}" | sort | uniq | tee ${dest}/pod-imagesId.txt || true
   kubectl describe pods --all-namespaces -l app.kubernetes.io/part-of=voltha > ${dest}/voltha-pods-describe.txt
