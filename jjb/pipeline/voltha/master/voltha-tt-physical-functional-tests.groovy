381e7b3ba89bfd1a3bf9b39bcdfac7ec22880c9a
Girish Gowdra <girish@opennetworking.org>
2021-10-28 15:12:45 -0700

    Copy dev_mgmt_daemon logs for TT tests
    Change-Id: Ia889362aca217febf42276b90acde3a6d2a61494

--- jjb/pipeline/voltha/master/voltha-tt-physical-functional-tests.groovy
+++ jjb/pipeline/voltha/master/voltha-tt-physical-functional-tests.groovy
@@ -210,7 +210,9 @@ pipeline {
           if (olt.type == null || olt.type == "" || olt.type == "openolt") {
              sh returnStdout: false, script: """
              sshpass -p ${olt.pass} scp ${olt.user}@${olt.sship}:/var/log/openolt.log $WORKSPACE/openolt-${olt.sship}.log || true
+             sshpass -p ${olt.pass} scp ${olt.user}@${olt.sship}:/var/log/dev_mgmt_daemon.log $WORKSPACE/dev_mgmt_daemon-${olt.sship}.log || true
              sed -i 's/\\x1b\\[[0-9;]*[a-zA-Z]//g' $WORKSPACE/openolt-${olt.sship}.log  # Remove escape sequences
+             sed -i 's/\\x1b\\[[0-9;]*[a-zA-Z]//g' $WORKSPACE/dev_mgmt_daemon-${olt.sship}.log  # Remove escape sequences
              """
           }
         }
