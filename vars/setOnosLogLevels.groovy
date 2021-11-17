6613359188fcb38252bc301d06eb277a62825f7f
Matteo Scandolo <matteo.scandolo@gmail.com>
2021-11-17 11:17:37 -0800

    Printing output of setOnosLogLevels to facilitate debugging
    Change-Id: I0cdbba6084206737f7d7fa7c46b833ffffb78824

--- vars/setOnosLogLevels.groovy
+++ vars/setOnosLogLevels.groovy
@@ -21,19 +21,16 @@ def call(Map config) {
     def instance = onosInstances.split('\n')[i]
       println "Setting log levels on ${instance}"
       sh """
-      set +x
       _TAG="onos-pf" bash -c "while true; do kubectl port-forward -n ${cfg.onosNamespace} ${instance} 8101; done"&
       """
 
       for (int j = 0; j < cfg.apps.size(); j++) {
         def app = cfg.apps[j]
         sh """
-        set +x
         sshpass -p karaf ssh -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -p 8101 karaf@localhost log:set ${cfg.logLevel} ${app}
         """
       }
       sh """
-        set +x
         ps e -ww -A | grep _TAG="onos-pf" | grep -v grep | awk '{print \$1}' | xargs --no-run-if-empty kill -9
       """
   }
