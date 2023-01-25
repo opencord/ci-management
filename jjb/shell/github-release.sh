fe28900b66d263484a493f15858d57e974cbd5d4
Joey Armstrong <joey@opennetworking.org>
2023-01-25 14:28:10 -0500

    Typo fix -- rsync should try copying from release/ (artifact_glob) not the original string 'release/*'
    Change-Id: I056eac16ed0d10187fccca4ce3f7a6375fea0f0c

--- jjb/shell/github-release.sh
+++ jjb/shell/github-release.sh
@@ -100,13 +100,15 @@ function doDebug()
     echo "** ${FUNCNAME[0]}: ARTIFACT_GLOB=${ARTIFACT_GLOB}"
     local artifact_glob="${ARTIFACT_GLOB%/*}"
     declare -p artifact_glob
+
+    echo "** ${FUNCNAME[0]}: PWD=$(/bin/pwd)"
     find "$artifact_glob" -print || /bin/true
 
     # Copy artifacts into the release temp dir
     # shellcheck disable=SC2086
     # cp -v "$ARTIFACT_GLOB" "$RELEASE_TEMP"
-    echo "rsync -rv --checksum \"$ARTIFACT_GLOB\" \"$RELEASE_TEMP/.\""
-    rsync -rv --checksum "$ARTIFACT_GLOB" "$RELEASE_TEMP/."
+    echo "rsync -rv --checksum \"$artifact_glob\" \"$RELEASE_TEMP/.\""
+    rsync -rv --checksum "$artifact_glob" "$RELEASE_TEMP/."
     
     echo
     echo "** ${FUNCNAME[0]}: RELEASE_TEMP=${RELEASE_TEMP}"
