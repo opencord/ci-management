66ed2f19da8990fad652b69ae9e1d775347fe94a
Joey Armstrong <joey@opennetworking.org>
2023-08-26 16:38:05 -0400

    groovy requires token True not true but lint did not catch
    Change-Id: I2804fadc1d6c35a786ae447a5572433f4361085c

--- vars/pkill_proc.groovy
+++ vars/pkill_proc.groovy
@@ -77,22 +77,23 @@ void call\
     String  proc,           // name of process or arguments to terminate
     Map     args=[:],
                             // Groovy, why special case list comma handling (?)
-    Boolean filler=True     // groovylint-disable-line UnusedMethodParameter
+    Boolean filler=true     // groovylint-disable-line UnusedMethodParameter
 ) {
     
     String iam = getIam('main')
-    Boolean ans = True
+    Boolean ans = true
 
     println("** ${iam}: ENTER")
 
     try {
         // Limit process matching by default
         if (! mymap.containsKey('command_only')) {
-            mymap['command_only'] = True
+            mymap['command_only'] = true
         }
         process(proc, args)
     }
     catch (Exception err) { // groovylint-disable-line CatchException
+        iam = 'pkill_proc.groovy'
         ans = False
         println("** ${iam}: EXCEPTION ${err}")
         throw err
