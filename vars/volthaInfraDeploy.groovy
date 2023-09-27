opencord/ci-management

commit 7b065235eeabe574ba46aef07360b52145af9bd8

deploy: set helm flags to deploy etcd and voltha ingress
* this is the first step to move from 'kubectl port-forward...' to the
  usage of kubernetes ingresses by enabling those without changing
  anything in terms of how the pipelines work (the new ingresses are
  not used yet)
* the used flags enable the helm deployment of ingresses for etcd and
  voltha and those can then be used by voltctl (once we render the
  fitting config) to read and write data
* note: there is no option to enable an ingress for kafka in the helm
  charts atm, so it might not be needed
* documentation for the flags can be found here:
  https://github.com/opencord/voltha-helm-charts#using-an-ingress-controller
* following commits need to:
  * generate a fitting voltctl configuration like documented here:
    https://github.com/opencord/voltha-helm-charts#usage-with-voltctl
  * make sure the DNS is configured fitting to the voltctl config
  * clean up the multiple instances of "kubectl port-forward..."

Change-Id: Ie4464fcf17bd3ca6775ee28a4f151d14ff25aff0
Signed-off-by: Jan Klare <jan.klare@bisdn.de>

vars/volthaInfraDeploy.groovy
@@ -147,6 +147,8 @@ kubectl create configmap -n ${cfg.infraNamespace} kube-config "--from-file=kube_
           --set kafka.replicaCount=${cfg.kafkaReplica},kafka.zookeeper.replicaCount=${cfg.kafkaReplica} \
           --set etcd.statefulset.replicaCount=${cfg.etcdReplica} \
           --set etcd.replicaCount=${cfg.etcdReplica} \
+          --set etcd.ingress.enabled=true
+          --set etcd.ingress.enableVirtualHosts=true
           -f $WORKSPACE/voltha-helm-charts/examples/${serviceConfigFile}-values.yaml ${cfg.extraHelmFlags}
 """)
 

vars/volthaStackDeploy.groovy
@@ -58,6 +58,8 @@ helm upgrade --install --create-namespace \
           --set global.stack_name=${cfg.stackName} \
           --set global.voltha_infra_name=voltha-infra \
           --set voltha.onos_classic.replicas=${cfg.onosReplica} \
+          --set voltha.ingress.enabled=true
+          --set voltha.ingress.enableVirtualHosts=true
           --set global.voltha_infra_namespace=${cfg.infraNamespace} \
           ${cfg.extraHelmFlags}
 """)

commit a4546e4357ea113fca1b403303a597053c8a4822

Add missing line continuation characters
Add the line continuation characters missing from the recent ingress
lines which resulted in this error:

  + --set etcd.ingress.enableVirtualHosts=true
  /w/workspace/periodic-software-upgrade-test-bbsim@tmp/durable-9565aa33/script.sh: line 3: --set: command not found

Fixes: 7b065235 ("deploy: set helm flags to deploy etcd and voltha ingress")
Signed-off-by: Roger Luethi <roger@opennetworking.org>
Change-Id: I9efd323b8771b22a267f4e0037dad90a30c1bbfd

vars/volthaInfraDeploy.groovy
@@ -147,8 +147,8 @@ kubectl create configmap -n ${cfg.infraNamespace} kube-config "--from-file=kube_
           --set kafka.replicaCount=${cfg.kafkaReplica},kafka.zookeeper.replicaCount=${cfg.kafkaReplica} \
           --set etcd.statefulset.replicaCount=${cfg.etcdReplica} \
           --set etcd.replicaCount=${cfg.etcdReplica} \
-          --set etcd.ingress.enabled=true
-          --set etcd.ingress.enableVirtualHosts=true
+          --set etcd.ingress.enabled=true \
+          --set etcd.ingress.enableVirtualHosts=true \
           -f $WORKSPACE/voltha-helm-charts/examples/${serviceConfigFile}-values.yaml ${cfg.extraHelmFlags}
 """)
 

vars/volthaStackDeploy.groovy
@@ -58,8 +58,8 @@ helm upgrade --install --create-namespace \
           --set global.stack_name=${cfg.stackName} \
           --set global.voltha_infra_name=voltha-infra \
           --set voltha.onos_classic.replicas=${cfg.onosReplica} \
-          --set voltha.ingress.enabled=true
-          --set voltha.ingress.enableVirtualHosts=true
+          --set voltha.ingress.enabled=true \
+          --set voltha.ingress.enableVirtualHosts=true \
           --set global.voltha_infra_namespace=${cfg.infraNamespace} \
           ${cfg.extraHelmFlags}
 """)