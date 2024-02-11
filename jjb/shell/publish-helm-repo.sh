518f35702f8b34266b977945ebd81f954db5b1d0
Joey Armstrong <joey@opennetworking.org>
2024-02-11 07:56:25 -0500

    Bulk update copyright notices to 2024
    Change-Id: I6e6d6adbe427332634e9253b9df6d070b4e1387d

--- jjb/shell/publish-helm-repo.sh
+++ jjb/shell/publish-helm-repo.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# SPDX-FileCopyrightText: 2020-2022 Open Networking Foundation <info@opennetworking.org>
+# SPDX-FileCopyrightText: 2022-2024 Open Networking Foundation (ONF) and the ONF Contributors
 # SPDX-License-Identifier: Apache-2.0
 
 set -eu -o pipefail
